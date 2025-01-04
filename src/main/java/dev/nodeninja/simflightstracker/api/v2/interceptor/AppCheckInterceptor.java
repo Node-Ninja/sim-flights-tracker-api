9b7a9f1d359de1423648243217e855206eb06729
NodeNinja <donaldsepeng@gmail.com>
2025-01-04 19:25:22 +0200

    firebase appcheck interceptor updates

--- src/main/java/dev/nodeninja/simflightstracker/api/v2/interceptor/AppCheckInterceptor.java
+++ src/main/java/dev/nodeninja/simflightstracker/api/v2/interceptor/AppCheckInterceptor.java
@@ -5,12 +5,15 @@ import com.auth0.jwk.JwkProvider;
 import com.auth0.jwk.JwkProviderBuilder;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import dev.nodeninja.simflightstracker.config.ApplicationConfigProperties;
 import dev.nodeninja.simflightstracker.exceptions.AuthenticationException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -21,7 +24,7 @@ import java.time.Duration;
 import java.util.Date;
 import java.util.List;
 
-
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class AppCheckInterceptor implements HandlerInterceptor {
@@ -29,8 +32,8 @@ public class AppCheckInterceptor implements HandlerInterceptor {
     private final ApplicationConfigProperties configProps;
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        String appCheckToken = request.getHeader("X-Firebase-Token");
+    public boolean preHandle(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull Object handler) {
+        String appCheckToken = request.getHeader("Authorization");
         var firebaseProjectName = configProps.getFirebase().getProjectName();
         var firebaseProjectNumber = configProps.getFirebase().getProjectNumber();
         var firebaseAppCheckKeyUrl = configProps.getFirebase().getAppCheckJwk();
@@ -70,6 +73,9 @@ public class AppCheckInterceptor implements HandlerInterceptor {
             algorithm.verify(tk);
 
             return true;
+        } catch (SignatureVerificationException e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return false;
         } catch (Exception e) {
             response.setStatus(HttpServletResponse.SC_FORBIDDEN);
             return false;
---
+++ src/main/java/dev/nodeninja/simflightstracker/config/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package dev.nodeninja.simflightstracker.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement()
+                .addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes(
+                        "Bearer Authentication", jwtSecurityScheme()
+                ))
+                .info(new Info()
+                        .title("SimFlight Tracker")
+                        .description("SFT APIs"));
+    }
+
+    public SecurityScheme jwtSecurityScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+    }
+}
