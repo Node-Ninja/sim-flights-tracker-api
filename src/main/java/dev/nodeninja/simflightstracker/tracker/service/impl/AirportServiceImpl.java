241b0896eaad30a9a6a0ed57797b5f844a78e4e0
NodeNinja <donaldsepeng@gmail.com>
2025-04-20 23:49:42 +0200

    limit the number of airports returned on search

--- src/main/java/dev/nodeninja/simflightstracker/tracker/service/impl/AirportServiceImpl.java
+++ src/main/java/dev/nodeninja/simflightstracker/tracker/service/impl/AirportServiceImpl.java
@@ -3,6 +3,8 @@ package dev.nodeninja.simflightstracker.tracker.service.impl;
 import dev.nodeninja.simflightstracker.api.v2.model.Airport;
 import dev.nodeninja.simflightstracker.tracker.repository.AirportRepository;
 import dev.nodeninja.simflightstracker.tracker.service.AirportService;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -23,6 +25,7 @@ public class AirportServiceImpl implements AirportService {
 
     @Override
     public List<Airport> searchAirports(String query) {
-        return airportRepository.findBy(query);
+        Pageable limit = PageRequest.of(0, 10);
+        return airportRepository.findBy(query, limit);
     }
 }
