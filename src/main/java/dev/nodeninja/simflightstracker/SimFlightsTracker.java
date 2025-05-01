71a245a2f53c0fa164c2f7ef479d72be57657a74
NodeNinja <donaldsepeng@gmail.com>
2025-05-01 13:59:25 +0200

    add filter for cache control headers

--- src/main/java/dev/nodeninja/simflightstracker/SimFlightsTracker.java
+++ src/main/java/dev/nodeninja/simflightstracker/SimFlightsTracker.java
@@ -4,11 +4,12 @@ import dev.nodeninja.simflightstracker.config.ApplicationConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@ServletComponentScan
 @EnableConfigurationProperties(ApplicationConfigProperties.class)
 public class SimFlightsTracker {
 	public static void main(String[] args) {
